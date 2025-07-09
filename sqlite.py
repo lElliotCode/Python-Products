f667cd4ae0c0aa0c4cfd8faa6ff7afc5c330bc49
Elliot <unforriven@gmail.com>
2025-07-09 18:12:45 -0300

    Más validaciones y fix algunos bugs

--- sqlite.py
+++ sqlite.py
@@ -1,4 +1,5 @@
 import sqlite3
+from stock import *
 from colorama import Fore, Back, init
 init(autoreset=True)
 
@@ -10,13 +11,13 @@ cursor = conexion.cursor()
 
 def create_table():
     cursor.execute('''
-    CREATE TABLE IF NOT EXISTS productos (
+    CREATE TABLE IF NOT EXISTS products (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre TEXT NOT NULL,
-    descripcion TEXT NOT NULL,
-    cantidad INTEGER NOT NULL,
-    precio REAL NOT NULL,
-    categoria TEXT NOT NULL)
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    stock INTEGER NOT NULL,
+    price REAL NOT NULL,
+    category TEXT NOT NULL)
     ''')
 
 def init_db():
@@ -30,7 +31,7 @@ def reset_database():
     if confirm != 's':
         print(Fore.RED + "Operación cancelada.")
         return
-    cursor.execute('''DROP TABLE IF EXISTS productos''')
+    cursor.execute('''DROP TABLE IF EXISTS products''')
     create_table()
     print(Fore.GREEN + "Base de datos reseteada correctamente")
     conexion.commit()
@@ -44,7 +45,7 @@ def add_product():
     category = input(Fore.CYAN + "Ingrese la categoria del producto: ")
 
     # Validaciones
-    if name == "" or price == "" :
+    if name == "" or price == "" or stock == "" or category == "":
         print(Fore.RED + "Error: No puede haber un campo vacío.")
         conexion.rollback()
         return
@@ -55,67 +56,70 @@ def add_product():
             conexion.rollback()
             return
     except ValueError:
-        print(Fore.RED + "Error: El precio/stock debe ser un número.")
+        print(Fore.RED + "Error: El precio debe ser un número válido.")
         conexion.rollback()
         return
-
+    # Validamos que el valor ingresa como stock sea un numero entero, no un float
+    try:
+        stock = int(stock)
+        if stock < 0:
+            print(Fore.RED + "Error: El stock debe ser mayor a 0.")
+            conexion.rollback()
+            return
+    except ValueError:
+        print(Fore.RED + "Error: El stock debe ser un número entero.")
+        conexion.rollback()
+        return
     cursor.execute('''
-    INSERT INTO productos (nombre, descripcion, precio, cantidad, categoria)
+    INSERT INTO products (name, description, stock, price, category)
     VALUES (?, ?, ?, ?, ?)
-    ''', (name, description, price, stock, category))
+    ''', (name, description, stock, price, category))
     print(Fore.GREEN + "Producto insertado correctamente \n")
     conexion.commit()
 
 # Mostrar datos
 def show_products():
-    cursor.execute('''SELECT * FROM productos''')
+    cursor.execute('''SELECT * FROM products''')
     productos = cursor.fetchall()
-
-    # Validaciones
     if len(productos) == 0:
         print(Fore.RED + "No hay productos")
-        conexion.rollback()
         return
-    
     for producto in productos:
         print(Fore.BLUE + f"ID: {producto[0]}\nNombre: {producto[1]}\nDescripción: {producto[2]}\nStock: {producto[3]}\nPrecio: ${producto[4]}\nCategoria: {producto[5]}\n")
     conexion.commit()
 
 def find_product():
-    cursor.execute('''SELECT * FROM productos''')
+    cursor.execute('''SELECT * FROM products''')
     productos = cursor.fetchall()
-
-    # Validaciones
     if len(productos) == 0:
         print(Fore.RED + "No hay productos")
-        conexion.rollback()
         return
     print(Fore.BLUE + "\n === Buscar producto ===")
-    campo = input(Fore.BLUE + "1. Buscar por ID\n2. Buscar por nombre\n3. Buscar por categoria\nIngrese el campo por el cual buscar: ")
+    field = input(Fore.BLUE + "1. Buscar por ID\n2. Buscar por nombre\n3. Buscar por categoria\nIngrese el campo por el cual buscar: ")
 
-    match campo:
+    match field:
         case "1":
             print(Fore.BLUE + "\n === Buscar por ID ===")
-            campo = "id"
-            valor = input(Fore.BLUE + "Ingrese el ID del producto: ")
+            field = "id"
+            value = input(Fore.BLUE + "Ingrese el ID del producto: ")
         case "2":
             print(Fore.BLUE + "\n === Buscar por nombre ===")
-            campo = "nombre"
-            valor = input(Fore.BLUE + "Ingrese el nombre del producto: ").lower()
+            field = "name"
+            value = input(Fore.BLUE + "Ingrese el nombre del producto: ").lower()
         case "3":
             print(Fore.BLUE + "\n === Buscar por categoria ===")
-            campo = "categoria"
-            valor = input(Fore.BLUE + "Ingrese la categoria del producto: ").lower()
+            field = "category"
+            value = input(Fore.BLUE + "Ingrese la categoria del producto: ").lower()
         case _:
             print(Fore.RED + "Error: Opcion invalida")
             return
 
-    if not valor:
+    if not value:
         print(Fore.RED + "Error: No se puede buscar un campo vacio")
         return
 
-    if campo == "id":
-        cursor.execute(f'''SELECT * FROM productos WHERE {campo} = ?''', (valor,))
+    if field == "id":
+        cursor.execute(f'''SELECT * FROM products WHERE {field} = ?''', (value,))
 
         producto = cursor.fetchone()
         if not producto:
@@ -124,7 +128,7 @@ def find_product():
         print(Fore.BLUE + f"\nID: {producto[0]}\nNombre: {producto[1]}\nDescripción: {producto[2]}\nStock: {producto[3]}\nPrecio: ${producto[4]}\nCategoria: {producto[5]}\n")
         conexion.commit()
     else:
-        cursor.execute(f'''SELECT * FROM productos WHERE {campo} LIKE ?''', ("%" + valor + "%",))
+        cursor.execute(f'''SELECT * FROM products WHERE {field} LIKE ?''', ("%" + value + "%",))
         productos = cursor.fetchall()
         if len(productos) == 0:
             print(Fore.RED + "No se encontro el producto")
@@ -135,12 +139,25 @@ def find_product():
 
 # Actualizar un dato
 def update_product():
+    # Validamos que la base de datos tiene productos
+    cursor.execute('''SELECT * FROM products''')
+    productos = cursor.fetchall()
+    if len(productos) == 0:
+        print(Fore.RED + "No hay productos")
+        return
+    
     id = input(Fore.BLUE + "Ingrese el ID del producto a actualizar: ")
-    name = input(Fore.BLUE + "Ingrese el nombre del producto: ")
-    description = input(Fore.BLUE + "Ingrese la descripcion del producto: ")
-    price = input(Fore.BLUE + "Ingrese el precio del producto: ")
-    stock = input(Fore.BLUE + "Ingrese el stock del producto: ")   
-    category = input(Fore.BLUE + "Ingrese la categoria del producto: ")
+
+    cursor.execute(f'''SELECT * FROM products WHERE id = ?''', (id,))
+    producto = cursor.fetchone()
+    if not producto:
+        print(Fore.RED + "No se encontro el producto")
+        return
+    name = input(Fore.BLUE + "Ingrese el nuevo nombre del producto: ")
+    description = input(Fore.BLUE + "Ingrese la nueva descripcion del producto: ")
+    stock = input(Fore.BLUE + "Ingrese el nuevo stock del producto: ")
+    price = input(Fore.BLUE + "Ingrese el nuevo precio del producto: ")   
+    category = input(Fore.BLUE + "Ingrese la nueva categoria del producto: ")
 
     # Validaciones
     if not name or not price or not stock or not category:
@@ -158,7 +175,7 @@ def update_product():
         return
 
     cursor.execute('''
-    UPDATE productos SET nombre = ?, descripcion = ?, precio = ?, cantidad = ?, categoria = ? WHERE id = ?
+    UPDATE products SET name = ?, description = ?, price = ?, stock = ?, category = ? WHERE id = ?
     ''', (name, description, price, stock, category, id))
 
     print(Fore.GREEN + "Producto actualizado correctamente \n")
@@ -168,8 +185,15 @@ def update_product():
 
 # Eliminar un dato
 def delete_product():
+    # Validamos que la base de datos tiene productos
+    cursor.execute('''SELECT * FROM products''')
+    productos = cursor.fetchall()
+    if len(productos) == 0:
+        print(Fore.RED + "No hay productos")
+        return
+    
     id = input(Fore.BLUE + "Ingrese el ID del producto a eliminar: ")
-    producto = cursor.execute('SELECT * FROM productos WHERE id = ?', (id,)).fetchall()
+    producto = cursor.execute('SELECT * FROM products WHERE id = ?', (id,)).fetchall()
 
     # Validaciones
     if not producto:
@@ -184,7 +208,7 @@ def delete_product():
         return
 
     cursor.execute('''
-    DELETE FROM productos WHERE id = ?
+    DELETE FROM products WHERE id = ?
     ''', (id,))
     print(Fore.YELLOW + f"Producto {id}: {producto[0][1]}, ${producto[0][2]} eliminado correctamente \n")
     conexion.commit()
--- stock.py
+++ stock.py
@@ -5,6 +5,12 @@ init(autoreset=True)
 # Variables
 min_quantity = 0
 
+# Conectamos a la base de datos
+conexion = sqlite3.connect("products.db")
+
+# Cursor para ejecutar las consultas
+cursor = conexion.cursor() 
+
 # Limite de stock para avisar al usuario
 def config_min_quantity():
     global min_quantity
@@ -12,14 +18,14 @@ def config_min_quantity():
     try:
         min_quantity = int(min_quantity)
         if min_quantity <= 0:
-            print(Fore.RED + "Error: El stock minimo debe ser mayor a 0.")
+            print(Fore.RED + "Error: El stock minimo debe ser un numero mayor a 0 y menor a 1000.")
             return
     except ValueError:
-        print(Fore.RED + "Error: El stock minimo debe ser un número.")
+        print(Fore.RED + "Error: El stock minimo debe ser un numero.")
         return
 
 def check_stock():
-    cursor.execute('''SELECT * FROM productos''')
+    cursor.execute('''SELECT * FROM products''')
     productos = cursor.fetchall()
 
     if len(productos) == 0:
@@ -35,8 +41,19 @@ def check_stock():
 def insertar_10_datos():
     for i in range(1, 10):
         cursor.execute('''
-        INSERT INTO productos (nombre, descripcion, precio, cantidad, categoria)
+        INSERT INTO products (name, description, stock, price, category)
         VALUES (?, ?, ?, ?, ?)
         ''', ("Producto " + str(i), "Descripcion " + str(i), i, i + i*10, "Categoria " + str(i)))
         print(Fore.GREEN + "Producto insertado correctamente \n")
         conexion.commit()
+
+# Con esta función validamos que la base de datos tenga productos antes de realizar una operación pero no funcionó
+# def validar_db():
+#     cursor.execute('''SELECT * FROM products''')
+#     productos = cursor.fetchall()
+
+#     # Validaciones
+#     if len(productos) == 0:
+#         print(Fore.RED + "No hay productos")
+#         conexion.rollback()
+#         return False
